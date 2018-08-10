2cdc83d6e88549fe2ab2fc39d25f60c40820d81b
MrEricL <eli6@stuy.edu>
2018-08-10 11:56:09 -0400

    tap remove working

--- App.js
+++ App.js
@@ -4,42 +4,54 @@ import Tags from 'react-native-tags';
 
 export default class App extends React.Component {
   render() {
+    let initTags = ['dog', 'cat', 'chicken'];
     return (
       <View style={styles.container}>
+        <Tags
+          ref={tags1 => (this.tags1 = tags1)}
+          initialText=""
+          initialTags={[]}
+          onChangeTags={tags => console.log(tags)}
+          onTagPress = {
+            (index, tagLabel) => {
+              console.log(this.tags1); 
+          }
+          }
+          containerStyle={{ justifyContent: 'center' }}
+          inputStyle={{ backgroundColor: 'white' }}
+        />
+ 
+        <Tags
+          ref={tags2 => (this.tags2 = tags2)}
+          initialText="monkey"
+          initialTags={initTags}
+          onChangeTags={tags => {
+            if (this.tags1) {
+              const oldTags = this.tags1.state.tags;
+              const tagThatWasAdded = this.tags2.state.tags.slice().pop();
+              const newTags = oldTags.slice();
+              newTags.push(`"${tagThatWasAdded}" added!`);
+              this.tags1.setState({ tags: newTags });
+            }
+          }}
+          onTagPress={(index, tagLabel) => {
+            if (this.tags1) {
+              //modifies the top tags
+              const oldTags = this.tags1.state.tags;
+              const newTags = oldTags.slice();
+              newTags.push(`"${tagLabel}" pressed!`);
+              this.tags1.setState({ tags: newTags });
 
-          <Tags
-            ref={(tags1) => this.tags1 = tags1}
-            initialText=""
-            initialTags={[]}
-            onChangeTags={tags => console.log(tags)}
-            containerStyle={{ justifyContent: 'center' }}
-            inputStyle={{ backgroundColor: 'white' }}
-          />
-
-          <Tags
-            ref={(tags2) => this.tags2 = tags2}
-            initialText="monkey"
-            initialTags={['dog', 'cat', 'chicken']}
-            onChangeTags={tags => {
-              if (this.tags1) {
-                const oldTags = this.tags1.state.tags;
-                const tagThatWasAdded = this.tags2.state.tags.slice().pop();
-                const newTags = oldTags.slice();
-                newTags.push(`"${tagThatWasAdded}" was added!`);
-                this.tags1.setState({tags: newTags}); 
-              }
-            }}
-            onTagPress={(index, tagLabel) => {
-              if (this.tags1) {
-                const oldTags = this.tags1.state.tags;
-                const newTags = oldTags.slice();
-                newTags.push(`"${tagLabel}" was pressed!`);
-                this.tags1.setState({tags: newTags});
-              }
-            }}
-            containerStyle={{ justifyContent: 'center' }}
-            inputStyle={{ backgroundColor: 'white' }}
-          />
+              //modifies the bottom tags
+              const oldTags2 = this.tags2.state.tags;
+              const newTags2 = oldTags2.slice();
+              newTags2.splice(index, 1);
+              this.tags2.setState({ tags: newTags2 });
+            }
+          }}
+          containerStyle={{ justifyContent: 'center' }}
+          inputStyle={{ backgroundColor: 'white' }}
+        />
       </View>
     );
   }
