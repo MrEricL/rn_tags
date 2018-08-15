b0352472cd54f019745e66e7b86f97b6a1ed6c16
MrEricL <eli6@stuy.edu>
2018-08-15 17:00:01 -0400

    midway point look here for revert

--- App.js
+++ App.js
@@ -1,17 +1,24 @@
 import React from "react";
 import { Text, View, StyleSheet, TextInput } from "react-native";
-import Tags from "react-native-tags";
+//import Tags from "react-native-tags";
+import Tags from "./react-native-tags"
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      custom: true,
+      initTags: ["dog", "cat", "chicken"],
+      initTagsCustom: ["dog", "cat", "chicken", "Add Custom"],
+    }
   }
 
   render() {
-    let initTags = ["dog", "cat", "chicken"];
+    
     return (
       <View style={styles.container}>
         <Tags
+          readonly = {true}
           ref={userTags => (this.userTags = userTags)}
           //tagContainerStyle = {styles.tagColor}
           initialText=""
@@ -19,7 +26,7 @@ export default class App extends React.Component {
           onChangeTags={tags => console.log()} //console.log(tags)}
           onTagPress={(index, tagLabel) => {
             if (this.userTags) {
-              if (initTags.includes(tagLabel)) {
+              if (this.state.initTags.includes(tagLabel)) {
                 const oldrecTags = this.recTags.state.tags;
                 const newrecTags = oldrecTags.slice();
                 newrecTags.push(tagLabel);
@@ -33,46 +40,36 @@ export default class App extends React.Component {
 
         <Tags
           ref={recTags => (this.recTags = recTags)}
-          //readonly = {true}
-          initialText="monkey"
-          initialTags={initTags}
+          readonly = {this.state.custom}
+          initialText=""
+          initialTags={this.state.initTagsCustom}
           onChangeTags={tags => {
             if (this.userTags) {
-              /*
-              const oldTags = this.userTags.state.tags;
-              const tagThatWasAdded = this.recTags.state.tags.slice().pop();
-              const newTags = oldTags.slice();
-              newTags.push(`${tagThatWasAdded} l`);
-              this.userTags.setState({ tags: newTags });
-
-              
-              const oldrecTags = this.recTags.state.tags;
-              const newrecTags = oldrecTags.slice();
-              console.log(newrecTags.slice())
-              console.log(newrecTags.slice(-1))
-              this.recTags.setState({ tags: newrecTags });
-              */
+              //Pass
             }
           }}
           onTagPress={(index, tagLabel) => {
             if (this.userTags) {
+
+              if (tagLabel == "Add Custom" ){
+                this.setState({custom: false});
+              }
+              else{
               //modifies the top tags
               const oldTags = this.userTags.state.tags;
               const newTags = oldTags.slice();
               newTags.push(`${tagLabel}`);
               this.userTags.setState({ tags: newTags });
 
-              //modifies the bottom tags
-              //const oldrecTags = this.recTags.state.tags;
-              //const newrecTags = oldrecTags.slice();
-              //newrecTags.splice(index, 1);
-              //this.recTags.setState({ tags: newrecTags });
+            }
+
+
             }
           }}
           containerStyle={{ justifyContent: "center" }}
           inputStyle={{ backgroundColor: "white" }}
         />
-      </View>
+      </View> 
     );
   }
 }
@@ -86,7 +83,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#ecf0f1",
     padding: 20
   },
-  tagColor: {
-    backgroundColor: "yellow"
-  }
+  //tagColor: {
+  //  backgroundColor: "yellow"
+  //}
 });
